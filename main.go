518b20652dfe266da5430a969ffda184987c3b1f
Abdul Wahid <abdul.wahid@synerzip.com>
2015-03-01 01:46:20 +0530

    Change port to 443

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 
 func main() {
 	router := NewRouter()
-	log.Fatal(http.ListenAndServe("https://secure-brook-4935.herokuapp.com", corsHandler(router))) 
+	log.Fatal(http.ListenAndServe(":443", corsHandler(router))) 
 
 }
